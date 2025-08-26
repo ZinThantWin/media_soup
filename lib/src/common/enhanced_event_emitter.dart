3e1c185a4416740204335c067c0ca0624af199a7
ZinThantWin <zinthantwin1997@gmail.com>
2025-08-26 14:32:23 +0630

    change web rtc version to 1

---
+++ lib/src/common/enhanced_event_emitter.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+
+import 'package:events2/events2.dart';
+import 'package:mediasoup_client_flutter/src/common/logger.dart';
+
+Logger _logger = Logger('EnhancedEventEmitter');
+
+class EnhancedEventEmitter extends EventEmitter {
+  EnhancedEventEmitter() : super();
+  void safeEmit(String event, [Map<String, dynamic>? args]) {
+    try {
+      emit(event, args);
+    } catch (error) {
+      _logger.error(
+        'safeEmit() event listener threw an error [event:$event]:$error',
+      );
+    }
+  }
+
+  Future<dynamic> safeEmitAsFuture(String event, [Map<String, dynamic>? args]) async {
+    final Completer<dynamic> completer = Completer<dynamic>();
+
+    try {
+      Map<String, dynamic> _args = {
+        'callback': completer.complete,
+        'errback': completer.completeError,
+        ...?args,
+      };
+      emitAsFuture(event, _args);
+      _logger.error(
+        'emitAsFuture called wow ${args?.keys}',
+      );
+
+      return completer.complete();
+    } catch (error) {
+      _logger.error(
+        'safeEmitAsFuture() event listener threw an error [event:$event]:$error',
+      );
+
+    }
+  }
+}
